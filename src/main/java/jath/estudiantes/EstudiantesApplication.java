ccd627f1f59b89755704d0d83ac9fc2a1149e470
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-27 12:06:23 -0600

    Configuracion de la capa de presentacion

--- src/main/java/jath/estudiantes/EstudiantesApplication.java
+++ src/main/java/jath/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,36 @@
 package jath.estudiantes;
 
+import jath.estudiantes.servicio.EstudianteServicio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
+		logger.info("Iniciando la aplicacion...");
+
+		//Levantar la fabrica de spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+
+		logger.info("Aplicacion finalizada!");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl + "Ejecutando metodo run de Spring" + nl);
+	}
 }
