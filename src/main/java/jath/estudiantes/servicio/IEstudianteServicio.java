2fa332f21c2c65f665c51b423df119f2bf2e780a
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-27 11:48:18 -0600

    Capa servicio

---
+++ src/main/java/jath/estudiantes/servicio/IEstudianteServicio.java
@@ -0,0 +1,12 @@
+package jath.estudiantes.servicio;
+
+import jath.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
+public interface IEstudianteServicio {
+    public List<Estudiante> listarEstudiantes();
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante);
+    public void guardarEstudiante(Estudiante estudiante);
+    public void eliminarEstudiante(Estudiante estudiante);
+}
